0e3023805204cecef6c1075fce4e0cf0fff8963b
Benedikt Berger <benedikt.berger@live.de>
2016-12-20 17:20:42 +0100

    added common module

--- src/ng2-image-gallery.module.ts
+++ src/ng2-image-gallery.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Ng2ImageGalleryComponent} from './ng2-image-gallery.component';
 import { DisplacerComponent } from './ng2-image-gallery-displacer.component';
 import { MaterialModule } from '@angular/material';
 
 @NgModule({
     imports: [
+        CommonModule,
         MaterialModule.forRoot(),
     ],
     declarations: [
