c4fe3ee5b40f4a80c33145f63905b70b2dd5b108
Benedikt Berger <benedikt.berger@live.de>
2017-08-10 10:17:25 +0200

    added fontawesome as dependency

--- src/ng2-image-gallery.module.ts
+++ src/ng2-image-gallery.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ng2ImageGalleryComponent} from './ng2-image-gallery.component';
 import { DisplacerComponent, DisplacerPortalDirective } from './ng2-image-gallery-displacer.component';
-import { MaterialModule } from '@angular/material';
+import { MdCoreModule } from '@angular/material';
 import { DragulaModule } from 'ng2-dragula';
 
 @NgModule({
     imports: [
         CommonModule,
         DragulaModule,
-        MaterialModule,
+        MdCoreModule,
     ],
     declarations: [
         DisplacerPortalDirective,
